5a1a2df1ed8b64cd02f670d0ef03733db13ddad6
ZZaRRyaDD <odddellarobea@gmail.com>
2021-12-04 00:57:45 +0700

    change url in tasks

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('task/user/<int:id>/', views.UserTasksView.as_view(), name="tasks_user"),
+    path('user/tasks/', views.UserTasksView.as_view(), name="tasks_user"),
     path('task/', views.UserTaskCreateView.as_view(), name="create_task"),
 ]
