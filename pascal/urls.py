e99ecfaad32bfb290147cf7dd9878bcecbcd0025
Alexey Bobrov <bobrov.bobrov-alex@yandex.ru>
2021-12-04 04:39:22 +0700

    asdf

--- pascal/urls.py
+++ pascal/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from project import views as project_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = routers.DefaultRouter()
 router.register('projects', project_views.ProjectViewSet)
@@ -30,3 +32,4 @@ urlpatterns = [
     path('api/v1/', include(router.urls)),
     # path('api/v1/', include('project.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
