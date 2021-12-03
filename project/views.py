186bad25a25c89fa897b1c39e32215ac7c25b1fe
Alexey Bobrov <bobrov.bobrov-alex@yandex.ru>
2021-12-03 22:46:43 +0700

    asdf

--- project/views.py
+++ project/views.py
@@ -23,11 +23,3 @@ class HintViewSet(viewsets.ModelViewSet):
     serializer_class = HintSerializer
     queryset = Hint.objects.all().select_related()
 
-
-def articles(request):
-    current_article = Article.objects.filter(pk=1).first().get_json()
-    other_articles = [a.get_json() for a in Article.objects.all()]
-    return JsonResponse({
-        "current_article": current_article,
-        "other_articles": other_articles,
-    }, status=200)
