ac00cc09253dc91d692243274fecfac5564a2371
ZZaRRyaDD <odddellarobea@gmail.com>
2021-12-04 00:56:32 +0700

    add users in db

--- pascal/settings.py
+++ pascal/settings.py
@@ -76,7 +76,7 @@ DJOSER = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=12),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=14),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
--- tasks/views.py
+++ tasks/views.py
@@ -3,12 +3,12 @@ from rest_framework import generics, permissions
 from . import models, serializers
 
 
-class UserTasksView(generics.RetrieveAPIView):
+class UserTasksView(generics.ListAPIView):
     permission_classes = [permissions.IsAuthenticated, ]
     serializer_class = serializers.UserTasksSerializer
 
     def get_queryset(self):
-        return models.UserTask.objects.filter(user__id=self.kwargs['id'])
+        return models.UserTask.objects.filter(user__id=self.request.user.id)
 
 
 class UserTaskCreateView(generics.CreateAPIView):
