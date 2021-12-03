a21b8107fbbaad9e22a6b1b6c8f235993695b32e
ZZaRRyaDD <odddellarobea@gmail.com>
2021-12-03 10:35:54 +0700

    create auth with jwt tokens

--- account/views.py
+++ account/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics, status, permissions, views
+from rest_framework.response import Response
+from . import serializers
 
-# Create your views here.
+
+class LogoutAPIView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated, ]
+    serializer_class = serializers.LogoutSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response({"message": "Пользователь вышел"},
+                        status=status.HTTP_204_NO_CONTENT)
--- pascal/settings.py
+++ pascal/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'backend-final-cp-21.herokuapp.com',
+    '127.0.0.1'
 ]
 
 
