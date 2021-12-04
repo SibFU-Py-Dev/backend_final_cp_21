1f93e7b402dc94793e0903937dd75d7c8679a8ae
Alexey Bobrov <bobrov.bobrov-alex@yandex.ru>
2021-12-04 10:40:18 +0700

    asdf

--- tasks/views.py
+++ tasks/views.py
@@ -4,14 +4,14 @@ from . import models, serializers
 
 
 class UserTasksView(generics.ListAPIView):
-    permission_classes = [permissions.IsAuthenticated, ]
+    # permission_classes = [permissions.IsAuthenticated, ]
     serializer_class = serializers.UserTasksSerializer
 
     def get_queryset(self):
-        return models.UserTask.objects.filter(user__id=self.request.user.id)
+        return models.UserTask.objects.all()
 
 
 class UserTaskCreateView(generics.CreateAPIView):
-    permission_classes = [permissions.IsAuthenticated, ]
+    # permission_classes = [permissions.IsAuthenticated, ]
     serializer_class = serializers.UserTasksSerializer
     queryset = models.UserTask.objects.all()
